0fc5b15a8b9c7b3d28d66ccd2e85fef73869db24
Shehzab Ahammad <shehzab@codecraft.co.in>
2020-08-05 20:04:25 +0530

    splash screen

---
+++ app/src/main/java/com/shehzabahammad/workout_timer/home/HomeActivity.kt
@@ -0,0 +1,12 @@
+package com.shehzabahammad.workout_timer.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.shehzabahammad.workout_timer.R
+
+class HomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
---
+++ app/src/main/java/com/shehzabahammad/workout_timer/splash/SplashScreenActivity.kt
@@ -0,0 +1,24 @@
+package com.shehzabahammad.workout_timer.splash
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.shehzabahammad.workout_timer.R
+import com.shehzabahammad.workout_timer.home.HomeActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    private var splashTimeOut = 3000L
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.myLooper()!!).postDelayed({
+            startActivity(Intent(this, HomeActivity::class.java))
+            finish()
+            overridePendingTransition(R.anim.slide_out_up, R.anim.slide_in_up)
+        }, splashTimeOut)
+    }
+}
