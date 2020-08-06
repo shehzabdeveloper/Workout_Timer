29a6698f7c54c34d7ba3aa552bf71b34e71fd964
Shehzab Ahammad <shehzab.developer@gmail.com>
2020-08-06 22:20:01 +0530

    fab button controls

--- app/src/main/java/com/shehzabahammad/workout_timer/home/screen/fragment/HomeFragment.kt
+++ app/src/main/java/com/shehzabahammad/workout_timer/home/screen/fragment/HomeFragment.kt
@@ -5,9 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.shehzabahammad.workout_timer.R
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
+    enum class ButtonState {
+        START_STATE, PAUSE_STATE, STOP_STATE
+    }
+
+    private lateinit var currentState: ButtonState
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -15,5 +22,46 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        currentState = ButtonState.START_STATE
+        fabPlay.setOnClickListener {
+            currentState = ButtonState.START_STATE
+            updateButtons()
+            Toast.makeText(context, "Clicked Play", Toast.LENGTH_SHORT).show()
+        }
+        fabPause.setOnClickListener {
+            currentState = ButtonState.PAUSE_STATE
+            updateButtons()
+            Toast.makeText(context, "Clicked Pause", Toast.LENGTH_SHORT).show()
+        }
+        fabStop.setOnClickListener {
+            currentState = ButtonState.STOP_STATE
+            updateButtons()
+            Toast.makeText(context, "Clicked Stop", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun updateButtons() {
+        when (currentState) {
+            ButtonState.START_STATE -> {
+                fabPlay.isClickable = false
+                fabPause.isClickable = true
+                fabStop.isClickable = true
+            }
+            ButtonState.PAUSE_STATE -> {
+                fabPlay.isClickable = true
+                fabPause.isClickable = false
+                fabStop.isClickable = true
+            }
+            ButtonState.STOP_STATE -> {
+                fabPlay.isClickable = true
+                fabPause.isClickable = false
+                fabStop.isClickable = false
+
+            }
+        }
     }
 }
